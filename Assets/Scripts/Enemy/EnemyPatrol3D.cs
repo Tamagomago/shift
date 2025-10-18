b6cb27cee74240369513a5820664a43c3a4868dd
ChocoCodes <chocojhong999@gmail.com>
2025-10-18 14:34:54 +0800

    chore: ai chase

--- Assets/Scripts/Enemy/EnemyPatrol3D.cs
+++ Assets/Scripts/Enemy/EnemyPatrol3D.cs
@@ -4,12 +4,16 @@ using System.Linq;
 
 public class EnemyPatrol3D : MonoBehaviour
 {
-    private bool isPaused = false;
-    private bool isChasing = false;
+    private bool _isPaused = false;
+    private bool _isChasing = false;
 
     [Header("Patrol Config")]
     [SerializeField] private float speed = 3f;
     [SerializeField] private Transform[] points;
+    [Header("Chase Config")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float chaseRange = 5f;
+    [SerializeField] private float loseRange = 5f;
 
     private Vector3[] _initialPointPos;
     private int patrolPointIdx = 0;
@@ -37,22 +41,53 @@ public class EnemyPatrol3D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isPaused || isChasing)
+        if (_isPaused)
         {
             agent.isStopped = true;
             return;
         }
 
+        DetectPlayer();
         Debug.Log($"{gameObject.name} | Agent Pos: {agent.transform.position:F2} | Target: {agent.destination:F2} | Remaining: {agent.remainingDistance:F2}");
-
-        agent.isStopped = false;
+        if(_isChasing)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+            return;
+        }
+        // agent.isStopped = false;
 
         if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance + 0.1f)
         {
             GoToNextPoint();
         }
     }
-    
+
+    private void DetectPlayer()
+    {
+        if (player == null) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (!_isChasing && distance <= chaseRange) StartChase();
+        else if (_isChasing && distance > loseRange) StopChase();
+    }
+
+    private void StartChase()
+    {
+        _isChasing = true;
+        agent.speed = speed + 1f;
+        Debug.Log($"{gameObject.name} started chasing {player.name}");
+    }
+
+    private void StopChase()
+    {
+        _isChasing = false;
+        agent.speed = speed;
+        GoToNextPoint();
+        Debug.Log($"{gameObject.name} stopped chasing and resumed patrol");
+    }
+
     private void GoToNextPoint()
     {
         if (points.Length < 2)

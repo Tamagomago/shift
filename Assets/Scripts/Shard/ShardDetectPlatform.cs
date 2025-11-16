593ff2c706089b310e6bf3210c3bf42cc3380cbf
ChocoCodes <chocojhong999@gmail.com>
2025-11-16 12:22:53 +0800

    fix: shard detect logic

--- Assets/Scripts/Shard/ShardDetectPlatform.cs
+++ Assets/Scripts/Shard/ShardDetectPlatform.cs
@@ -6,15 +6,28 @@ public class ShardDetectPlatform : MonoBehaviour
     [SerializeField] private float shardRadius = 2f; // radius to detect nearby platforms
     private DimensionManager _dimensionManager;
     private ShardPlayerFollow _shardPlayerFollow;
+    
+    [Header("Shard Glow")]
+    [SerializeField] private GameObject playerShardObject;
+    [SerializeField] private float baseEmissionIntensity = 10f;
+    [SerializeField] private float glowingEmissionIntensity = 50f;
+    [SerializeField] private Color emissionColor = new Color(0.749f, 0.749f, 0.749f);
+    private Material _shardMaterial;
+    private static readonly int EmissionColorID = Shader.PropertyToID("_EmissionColor");
     private void Awake()
     {
         _dimensionManager = FindFirstObjectByType<DimensionManager>();
         _shardPlayerFollow = GetComponent<ShardPlayerFollow>();
+        Renderer shardRenderer = playerShardObject.GetComponent<Renderer>();
+        _shardMaterial = shardRenderer.material;
+        _shardMaterial.EnableKeyword("_EMISSION");
+        
         if (_dimensionManager == null || _shardPlayerFollow == null)
         {
             Debug.LogWarning("DimensionManager or ShardPlayerFollow is not found in the Shard GameObject!");
             return;
         }
+        SetEmission(baseEmissionIntensity);
     }
 
     private void Update()
@@ -23,7 +36,14 @@ public class ShardDetectPlatform : MonoBehaviour
         GameObject[] realmToCheck = _dimensionManager.GetOppositeRealmPlatforms();
         bool hasNearbyPlatforms = CheckNearbyPlatforms(realmToCheck);
 
-        // TODO ADD GLOW EFFECT HERE:
+        float intensityToApply = hasNearbyPlatforms ? glowingEmissionIntensity : baseEmissionIntensity;
+        SetEmission(intensityToApply);
+    }
+
+    private void SetEmission(float intensity)
+    {
+        Color finalColor = emissionColor * intensity;
+        _shardMaterial.SetColor(EmissionColorID, finalColor);
         string result = hasNearbyPlatforms ? "Player is near a platform" : "No platform nearby player.";
         Debug.Log(result);
     }
